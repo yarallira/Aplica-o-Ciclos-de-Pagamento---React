26c86896912eb68653c567df033650ee746435d3
cbykuser <yarallira555@gmail.com>
2020-02-27 16:07:27 -0300

    Desenvolvimento

---
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,5 @@
+const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, runValidators: true})
+module.exports = BillingCycle
